387bb9749b5b099fd472f07697ebd1a9dce37eaa
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-04-26 10:57:27 +0200

    Implement test for active friend with autospec

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
 from unittest import TestCase, expectedFailure
-from unittest.mock import Mock
+from unittest.mock import create_autospec
 
 from api import PejsbookApi
 from app import App
@@ -7,7 +7,7 @@ from app import App
 
 class TestApp(TestCase):
     def test_that_poking_a_friend_sends_a_private_message_when_friend_is_active(self):
-        api = Mock(spec=PejsbookApi)
+        api = create_autospec(spec=PejsbookApi)
         api.check_user_status.return_value = 'active'
         app = App(api)
         friend = "Ryan Gosling"
