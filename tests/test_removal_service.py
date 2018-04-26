171f96f08862c361ab0ee29bff92c51d7dfb4aa7
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-04-26 09:26:15 +0200

    Add initial test for existing dir

--- tests/test_removal_service.py
+++ tests/test_removal_service.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from removal_service import remove_file
 
@@ -7,3 +8,13 @@ class TestRemovalFile(TestCase):
     def test_that_non_existing_file_is_not_deleted(self):
         non_existing_file = "sadsadsa.exe"
         remove_file(non_existing_file)
+
+    @patch("os.path.exists", return_value=True)
+    @patch("os.path.isdir", return_value=True)
+    def test_that_existing_directory_is_not_deleted(self, isdir_mock, exists_mock):
+        existing_dir = "/tmp"
+
+        remove_file(existing_dir)
+
+        exists_mock.assert_called_once_with(existing_dir)
+        isdir_mock.assert_called_once_with(existing_dir)
