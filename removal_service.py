6146e8d78e61edf7e831d066bf8558f3fd57ffc0
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-04-26 09:21:16 +0200

    Extend remove function

--- removal_service.py
+++ removal_service.py
@@ -2,5 +2,8 @@ import os
 
 
 def remove_file(filename):
-    if os.path.isfile(filename):
-        os.remove(filename)
+    if os.path.exists(filename):
+        if os.path.isfile(filename):
+            os.remove(filename)
+        elif os.path.isdir(filename):
+            print("Cannot delete directory with this function")
