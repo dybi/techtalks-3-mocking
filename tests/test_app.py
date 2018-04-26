9aa095991ad15faf16abd2e5c01df82d730b7ff1
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-04-26 10:49:27 +0200

    Implement test for active friend with Mock and spec

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,19 @@
 from unittest import TestCase, expectedFailure
 from unittest.mock import Mock
 
+from api import PejsbookApi
 from app import App
 
 
 class TestApp(TestCase):
     def test_that_poking_a_friend_sends_a_private_message_when_friend_is_active(self):
-        api = Mock()
+        api = Mock(spec=PejsbookApi)
         api.check_user_status.return_value = 'active'
         app = App(api)
         friend = "Ryan Gosling"
         app.poke_a_friend(friend)
         api.check_user_status.assert_called_once_with(friend)
-        api.send_private_message.assert_caled_once_with(friend, "Fancy a beer?")
+        api.send_private_message.assert_called_once_with(friend, "Fancy a beer?")
 
     @expectedFailure
     def test_that_poking_a_friend_posts_to_a_friends_wall_when_friend_is__inactive(self):
