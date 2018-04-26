261676ddb50694ef7c9c21795e4920acfde5601a
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-04-26 10:32:48 +0200

    Add tests stubs fr api based app

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestApp(TestCase):
+    def test_that_poking_a_friend_sends_a_private_message_when_friend_is_active(self):
+        pass
+
+    def test_that_poking_a_friend_posts_to_a_friends_wall_when_friend_is__inactive(self):
+        pass
+
+    def test_that_poking_neither_posts_to_a_wall_nor_sendS_private_message_when_poking_stranger(self):
+        pass
