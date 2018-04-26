8e5361384553d4d9b01769bd628a46bff06fe69c
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-04-26 10:58:08 +0200

    Adapt app to new API

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ class App(object):
 
     def poke_a_friend(self, friend_name):
         if self.api.check_user_status(friend_name) == 'active':
-            self.api.send_private_message(friend_name, "Fancy a beer?")
+            self.api.send_private_message(friend_name, "Fancy a beer?", 0)
         elif self.api.check_user_status(friend_name) == 'inactive':
             self.api.post_to_friends_wall(friend_name, "We miss you!111")
 
